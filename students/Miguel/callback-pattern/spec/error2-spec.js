92da8c01f1f049c153872ee8d42df8c598ec9f8c
Miguel Delgado <miguedelgado1324@gmail.com>
2016-11-25 15:52:59 -0600

    Solving errors and testing them, Synchronous and Asynchronous functions and tests

---
+++ students/Miguel/callback-pattern/spec/error2-spec.js
@@ -0,0 +1,46 @@
+"use strict";
+
+let doSomeComplicatedMath = require('../error2.js');
+let promise;
+let functionRes;
+const a = 2;
+const b = 3;
+const c = 7;
+
+function injectedFunction (err, res){
+    if (err) {
+      console.error('Some nasty error');
+      return;
+    }
+
+    let result = res;
+
+    return result;
+}
+
+describe('Testing function doSomeComplicatedMath', () => {
+    beforeEach(() => {
+        promise = new Promise((resolve, reject) => {
+            resolve(doSomeComplicatedMath(a, b, c, injectedFunction));
+        });
+    });
+
+    it('Should be defined', () => {
+        expect(doSomeComplicatedMath).toBeDefined();
+    });
+    it('Should be a function', () => {
+        expect(typeof doSomeComplicatedMath).toBe("function");
+    });
+    it('Should return a number', () => {
+        promise.then((data) => {
+            functionRes = data;
+            expect(typeof functionRes).toBe("number");
+        });
+    });
+    it('The result should be 2.4285714285714284', () => {
+        promise.then((data) => {
+            functionRes = data;
+            expect(functionRes).toBe(2.4285714285714284);
+        });
+    });
+});
