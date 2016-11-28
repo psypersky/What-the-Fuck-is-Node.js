db574ab7e23868cb0fd3281c2003309cdf4c441a
fuksia <sara.fuksia@hotmail.com>
2016-11-27 23:17:52 -0600

    I have nothing to say

--- students/Sara/callback-pattern/error2.js
+++ students/Sara/callback-pattern/error2.js
@@ -1,22 +1,25 @@
+"use strict";
 
 const a = 2;
 const b = 3;
 const c = 7;
 
 let result;
-doSomeComplicatedMath(a, b, c, (err, res) => {
+
+function doSomeComplicatedMath(a, b, c, callback) {
+  const res = a + b / c;
+  process.nextTick(function() {
+    callback(null, res);
+    console.trace("nextTick");
+  });
+}
+
+doSomeComplicatedMath(a, b, c, function (err, res) {
   if (err) {
     console.error('Some nasty error');
     return;
   }
+  console.trace("doSomeComplicatedMath");
   result = res;
+  console.log('Result', result);
 });
-
-console.log('Result', result);
-
-function doSomeComplicatedMath(a, b, c, callback) {
-  const res = a + b / c;
-  process.nextTick(() => {
-    callback(null, res);
-  });
-}
