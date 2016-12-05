488c14079ad4bacc6df2cce9f698cce78766c8de
Axel Nolasco <axl_nolasco@hotmail.com>
2016-12-05 16:38:58 -0600

    Axel's exercises with server CRUD

---
+++ students/axel/ServerResponse/zipFile.js
@@ -0,0 +1,24 @@
+var http = require('http'),
+    zlib = require('zlib'),
+    fs   = require('fs');
+// console.log(http.STATUS_CODES);
+
+http.createServer(function (req, res) {
+    console.log("Method: " + req.method);
+    res.writeHead(200, {
+        'Content-Type': 'text/html'
+    });
+    res.write("<h1>Welcome home, cowboy!</h1><br>");
+    req.on('data', function (data) {
+        console.log("data: " + data);
+        zlib.gzip(data, function (_, result) {  // The callback will give you the
+            console.log("res :" + result);
+            fs.writeFile('newCompresFile.zip', result);
+            res.write(result);
+            res.end();                     // result, so just send it.
+        });
+    });
+    // res.end();
+}).listen(3000, "localhost");
+
+console.log("Server created at port 3000");
