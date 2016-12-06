03cde2f57a84db7e79ae75c493eb9e32ecb7480e
Miguel Delgado <miguedelgado1324@gmail.com>
2016-12-06 11:45:25 -0600

    Severs working, i think :P

--- students/Miguel/serverRequests/newServer.js
+++ students/Miguel/serverRequests/newServer.js
@@ -9,44 +9,31 @@ let server = http.createServer(function (request, response){
   let url = request.url;
   let headers = request.headers;
   let flag = true;
-
   request.on('data', function (chunk){
     body.push(chunk);
   }).on('end', function (){
-
-
     flag = url.match('/sum');
-
     if(flag && method === "GET" && flag['index'] === 0 && (url[4] === undefined || url[4] === "/")){
       response.on('error', function (err){
         console.error(err);
       });
-
       response.statusCode = 200;
-
       response.setHeader('Content-type', 'application/json');
-
       responseBodyArray = url.split("/");
       responseBodyArray.shift();
       responseBodyArray.shift();
       responseBody += responseBodyArray.join("/");
-
       response.write(responseBody);
       response.end();
     } else {
       response.on('error', function (err){
         console.error(err);
       });
-
       response.statusCode = 404;
-
       response.write('Error 404: Route not found');
-
       response.end();
     }
-
   }).on('error', function(err){
     console.error(err.stack);
   });
-
 }).listen(8080);
---
+++ students/Miguel/serverRequests/writeFileZipPOSTMethod.js
@@ -0,0 +1,29 @@
+"use strict";
+
+let http = require('http');
+let fs = require('fs');
+let gzip = require('zlib').createGzip();
+
+let server = http.createServer(function (request, response){
+  if(request.method === "POST"){
+    console.log(fs.createReadStream);
+    response.writeHead(200, {'Content-Type': 'application/zip', 'Content-disposition': 'attachment; filename=myFile.zip'});
+    let myFile = fs.createWriteStream("fileToBeCompressed.txt");
+    let fileCompressed = fs.createWriteStream("fileCompressed.zip");
+    request.pipe(myFile);
+    let fileGzip = request.pipe(gzip);
+    fileGzip.pipe(fileCompressed);
+    fileGzip.pipe(response);
+  } else {
+      let responseText;
+      response.writeHead(200, {'Content-type': 'text/html'});
+      fs.readFile('index.html', function (err, data){
+        if (err) {
+          throw err;
+        }
+        responseText = data;
+        response.write(responseText);
+        response.end();
+      });
+  }
+}).listen(8080);
