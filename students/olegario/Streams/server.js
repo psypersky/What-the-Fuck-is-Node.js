b912274fa6d67a1d4c628af63c3074b625dc9617
shockolegario <olegario_90_shock@hotmail.com>
2016-12-08 19:14:25 -0600

    ident code

--- students/olegario/Streams/server.js
+++ students/olegario/Streams/server.js
@@ -3,9 +3,9 @@
 var http = require('http');
 var fs   = require('fs');
 
-  http.createServer(function (req, res) {
-    if(req.method == 'GET'){
-      res.writeHead(200,'Content-Type', 'text/html');
-      fs.createReadStream('./index.html').pipe(res);
-    }
-  }).listen(8080);
+http.createServer(function (req, res) {
+  if(req.method == 'GET'){
+    res.writeHead(200,'Content-Type', 'text/html');
+    fs.createReadStream('./index.html').pipe(res);
+  }
+}).listen(8080);
