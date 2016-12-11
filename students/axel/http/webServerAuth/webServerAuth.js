c54913b0aa652cac962f9d8094c15a9c178c3d8d
Axel Nolasco <axl_nolasco@hotmail.com>
2016-12-11 15:06:27 -0600

    Going up!

--- students/axel/http/webServerAuth/webServerAuth.js
+++ students/axel/http/webServerAuth/webServerAuth.js
@@ -4,7 +4,7 @@ var http = require('http');
 fs   = require('fs');
 
 http.createServer(function (req, res) {
-    
+
     var dataBase = [
         {
             'user' : 'Ruben',
@@ -57,7 +57,7 @@ http.createServer(function (req, res) {
 
             data.forEach(function(element){
                 userPass.push(element.split("="));
-            });
+        });
 
             user = userPass[0][1];
             pass = userPass[1][1];
