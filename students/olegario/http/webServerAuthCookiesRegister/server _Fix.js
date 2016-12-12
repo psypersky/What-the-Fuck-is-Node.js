16ea763c5b2a5f781d76b553ce63c6f1038c8c37
shockolegario <olegario_90_shock@hotmail.com>
2016-12-12 17:25:48 -0600

    changing the name for the file corrected

--- students/olegario/http/webServerAuthCookiesRegister/server _Fix.js
+++ students/olegario/http/webServerAuthCookiesRegister/server _Fix.js
@@ -1,12 +1,13 @@
 "use strict"
 
-let http = require('http');
+let http        = require('http');
 let querystring = require('querystring');
-var setCookie = require('set-cookie-parser');
-let cookie = require('cookie');
-let fs = require('fs');
+var setCookie   = require('set-cookie-parser');
+let cookie      = require('cookie');
+let fs          = require('fs');
 
-let db = [
+//Database for the example
+let database = [
   {
     username : 'Ruben',
     pass     : 'MyPass'
@@ -22,52 +23,55 @@ let db = [
 ];
 
 http.createServer(function(req, res){
-  let cookies = setCookie.parse(res);
 
+  let cookies    = setCookie.parse(res);
+
+  // parse the Cookie content to mainCookie
   let mainCookie = cookie.parse(req.headers.cookie || '');
 
+  //check if exist some cookie
   if (mainCookie.username) {
-    if((req.method == 'GET') && (req.url === '/signUp')){
+    if((req.method == 'GET') && (req.url === '/signUp')){//if exist some cookie and want to sign up, I send to welcome page
       fs.readFile('./welcome.html', function (err, data) {
         res.writeHead(200, {'Content-Type': 'text/html'});
         res.end(data);
       });
     }
 
-    else if((req.method == 'GET') && (req.url === '/login')){
+    else if((req.method == 'GET') && (req.url === '/login')){//if exist some cookie and want to login again, I send to welcome page
       fs.readFile('./welcome.html', function (err, data) {
         res.writeHead(200, {'Content-Type': 'text/html'});
         res.end(data);
       });
     }
 
-    else if((req.method == 'GET') && (req.url === '/user')){
+    else if((req.method == 'GET') && (req.url === '/user')){//if exist some cookie and want go to user page, I send to welcome page
       fs.readFile('./welcome.html', function (err, data) {
         res.writeHead(200, {'Content-Type': 'text/html'});
         res.end(data);
       });
     } else {
-      fs.readFile('./notFound.html', function (err, data) {
+      fs.readFile('./notFound.html', function (err, data) {//if exist some cookie and want go to another route, I send to error page
         res.writeHead(404, {'Content-Type': 'text/html'});
         res.end(data);
       });
     }
-  }  else {
+  }  else { //if doesn't exist some cookie, then somebody want to login or sign up
 
-    if((req.method == 'GET') && (req.url === '/signUp')){
+    if((req.method == 'GET') && (req.url === '/signUp')){//if doesn't exist some cookie and want sign up, send the sign up page
       fs.readFile('./sign.html', function (err, data) {
         res.writeHead(200, {'Content-Type': 'text/html'});
         res.end(data);
       });
     }
 
-    else if((req.method == 'GET') && (req.url === '/login')){
+    else if((req.method == 'GET') && (req.url === '/login')){//if doesn't exist some cookie and want login, send the login page
       fs.readFile('./login.html', function (err, data) {
         res.writeHead(200, {'Content-Type': 'text/html'});
         res.end(data);
       });
     }
-
+    //if somebody want sign up, verify that your inputs to be correct
     else if((req.method == 'POST') && (req.url === '/validate')){
       let body = '', form;
 
@@ -80,9 +84,9 @@ http.createServer(function(req, res){
 
         if (form.test) {
           let errors = [];
-          let name  = form.name;
-          let email = form.mail;
-          let passw = form.pass
+          let name   = form.name;
+          let email  = form.mail;
+          let passw  = form.pass
 
           if( name === null || name.length === 0) {
             errors.push("err1");
@@ -110,7 +114,7 @@ http.createServer(function(req, res){
         }
       });
     }
-
+    //if somebody want sign up and your inputs to be correct, I register him and I login him
     else if((req.method == 'POST') && (req.url === '/signUp')){
       let body = '', form;
 
@@ -121,7 +125,7 @@ http.createServer(function(req, res){
       req.on('end', function(data){
         let form = querystring.parse(body);
 
-        db.push({
+        database.push({
           name  : form.user,
           email : form.mail,
           phone : form.phone,
@@ -135,7 +139,7 @@ http.createServer(function(req, res){
         });
       });
     }
-
+    //if somebody want login, send the login page
     else if((req.method == 'POST') && (req.url === '/login')){
       let body = '', form;
 
@@ -144,18 +148,16 @@ http.createServer(function(req, res){
       });
 
       req.on('end', function(data){
-        form = querystring.parse(body);
+        form     = querystring.parse(body);
 
-        let a = db.findIndex(function (element) {
-          if (element.username === form.user) {
-            return true;
-          }
+        let user = database.find(function (element) {
+          return  (element.username === form.user);
         });
 
-        if (a >= 0) {
-          if (form.pass === db[a].pass) {
+        if (user) {//if the user exist in the database
+          if (form.pass === user.pass) {// check that the pass to be equal to the pass in the database
             fs.readFile('./welcome.html', function (err, data) {
-              res.setHeader('Set-Cookie', ['username=' + db[a].username]);
+              res.setHeader('Set-Cookie', ['username=' + user.username]);
               res.writeHead(200, {'Content-Type': 'text/html'});
               res.end(data);
             });
@@ -172,7 +174,7 @@ http.createServer(function(req, res){
           });
         }
       });
-    } else {
+    } else {//when somebody want to go to another route, send the error page
       fs.readFile('./notFound.html', function (err, data) {
         res.writeHead(404, {'Content-Type': 'text/html'});
         res.end(data);
